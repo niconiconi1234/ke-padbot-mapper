386b466d5c4d5b65c25f6304b612a8074cfc2020
Haojie Zhang <zhj630985214@gmail.com>
2023-08-23 19:36:29 +0800

    0.0.2

--- scripts/main.py
+++ scripts/main.py
@@ -8,13 +8,20 @@ from entities import DeviceProfiles, DeviceStateUpdate, DeviceTwinUpdate, MsgTwi
 import time
 import requests
 from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor
+from paho.mqtt import subscribe
+from threading import Thread
+import time
 
 PADBOT_STATUS_URL = os.environ['PADBOT_STATUS_URL']
+PADBOT_NAVIGATION_URL = os.environ['PADBOT_NAVIGATION_URL']
 DEVICE_NAME = os.environ['DEVICE_NAME']
 
 s_batteryPercentage = "batteryPercentage"
 s_batteryStatus = "batteryStatus"
 s_actionStatus = "actionStatus"
+s_navigationStatus = "navigationStatus"
+s_robotLocation = "robotLocation"
 
 DeviceETPrefix = "$hw/events/device/"
 DeviceETStateUpdateSuffix = "/state/update"
@@ -61,44 +68,64 @@ class PadBotMapper:
         state_update_url = DeviceETPrefix + str(self.device_id) + DeviceETStateUpdateSuffix
         self.client.publish(state_update_url, msg_json)
 
+    def subscribe(self):
+        topic = DeviceETPrefix + str(self.device_id) + TwinETGetResultSuffix
+        msg = subscribe.simple(topic, hostname=MQTT_HOST, port=MQTT_PORT)
+        return DeviceTwinUpdate(**json.loads(msg.payload))
+
     def create_actual_update_msg(self,
                                  batteryPercentage,
                                  batteryStatus,
-                                 actionStatus):
-        """
-        需要向云端更新机器人状态时，创建更新消息(包括电量百分比、电量状态、动作状态)
-        """
+                                 actionStatus,
+                                 navigationStatus,
+                                 robotLocation):
         logging.info('Creating actual update message')
-        actionStatus_update_msg = DeviceTwinUpdate(
+        msg = DeviceTwinUpdate(
             twin={
                 s_actionStatus: MsgTwin(actual=TwinValue(value=actionStatus), metadata=TypeMetadata(type="Updated")),
-            }
-        )
-        batteryPercentage_update_msg = DeviceTwinUpdate(
-            twin={
                 s_batteryPercentage: MsgTwin(actual=TwinValue(value=batteryPercentage), metadata=TypeMetadata(type="Updated")),
-            }
-        )
-        batteryStatus_update_msg = DeviceTwinUpdate(
-            twin={
-                s_batteryStatus: MsgTwin(actual=TwinValue(value=batteryStatus), metadata=TypeMetadata(type="Updated"))
+                s_batteryStatus: MsgTwin(actual=TwinValue(value=batteryStatus), metadata=TypeMetadata(type="Updated")),
+                s_robotLocation: MsgTwin(actual=TwinValue(value=robotLocation), metadata=TypeMetadata(type="Updated")),
+                s_navigationStatus: MsgTwin(actual=TwinValue(value=navigationStatus), metadata=TypeMetadata(type="Updated"))
             }
         )
 
-        return actionStatus_update_msg, batteryPercentage_update_msg, batteryStatus_update_msg
+        return msg
 
     def get_twin(self, updateMessage: DeviceTwinUpdate):
         get_twin_topic = DeviceETPrefix + str(self.device_id) + TwinETGetSuffix
         self.client.publish(get_twin_topic, updateMessage.model_dump_json())
 
-    def loop_once(self):
+    def loop_once(self, updateMessage: DeviceTwinUpdate):
         """
-        向云端上报机器人状态
-        首先调用请求PADBOT_STATUS_URL获取机器人状态, 再将状态上报到云端
+        从云端获取期望值, 并操作派宝机器人导航
+        再从机器人获取实际值, 并更新到云端
         """
+        with ThreadPoolExecutor(max_workers=3) as executor:
+            future = executor.submit(self.subscribe)  # subscribe expected value from cloud
+            time.sleep(1)  # to ensure when we send request to get expected value on the next line, we have subscribed the mqtt topic to get expected value
+            self.get_twin(updateMessage)  # send request to get expected value from cloud
+            device_twin_result = future.result()  # get expected value from cloud
+
+        # 根据期望的位置值操作派宝机器人导航，首先获得期望位置和实际位置
+        expected = device_twin_result.twin[s_robotLocation].expected
+        expectedLocation = expected.value if expected else None
         rsp = requests.get(PADBOT_STATUS_URL)
         if rsp.status_code != 200:
             logging.error('Failed to get padbot status from %s, error code: %s', PADBOT_STATUS_URL, rsp.status_code)
+            return
+        actualLocation = rsp.json().get('robotLocation', None)
+
+        # 如果actualLocation和expectedLocation不一致, 且机器人不在移动中, 且expectedLocation不为未知, 则开始导航
+        if expectedLocation and expectedLocation != actualLocation and actualLocation != 'MOVING' and expectedLocation != 'UNKNOWN':
+            # 开始导航，因为这个http请求会阻塞，直到导航结束，所以我们开一个新的线程来发送请求
+            t = Thread(target=requests.post, args=(PADBOT_NAVIGATION_URL, ), kwargs={'json': {'targetPoint': expectedLocation}})
+            t.start()
+
+        rsp = requests.get(PADBOT_STATUS_URL)
+        if rsp.status_code != 200:
+            logging.error('Failed to get padbot status from %s, error code: %s', PADBOT_STATUS_URL, rsp.status_code)
+            return
 
         padbotStatus = rsp.json()
         padbotStatus = defaultdict(lambda: 'UNKNOWN', padbotStatus)
@@ -106,14 +133,14 @@ class PadBotMapper:
         batteryPercentage = str(padbotStatus['batteryPercentage'])
         batteryStatus = padbotStatus['batteryStatus']
         actionStatus = padbotStatus['actionStatus']
+        navigationStatus = padbotStatus['navigationStatus']
+        robotLocation = padbotStatus['robotLocation']
 
-        logging.info('Got padbot status: batteryPercentage: %s, batteryStatus: %s, actionStatus: %s',
-                     batteryPercentage, batteryStatus, actionStatus)
+        logging.info('Got padbot status: batteryPercentage: %s, batteryStatus: %s, actionStatus: %s, navigationStatus: %s, robotLocation: %s',
+                     batteryPercentage, batteryStatus, actionStatus, navigationStatus, robotLocation)
 
-        update_msgs = self.create_actual_update_msg(batteryPercentage, batteryStatus, actionStatus)
-        for msg in update_msgs:
-            self.change_twin_value(msg)
-
+        update_msgs = self.create_actual_update_msg(batteryPercentage, batteryStatus, actionStatus, navigationStatus, robotLocation)
+        self.change_twin_value(update_msgs)  # send actual value to cloud
         logging.info('Update message sent to cloud')
 
     def change_twin_value(self, updateMessage: DeviceTwinUpdate):
@@ -124,8 +151,9 @@ class PadBotMapper:
 def main():
     mapper = PadBotMapper()
     mapper.change_device_state("online")  # change device state to online
+    msg = mapper.create_actual_update_msg('Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown')
     while True:
-        mapper.loop_once()
+        mapper.loop_once(msg)
         time.sleep(1)
 
 
